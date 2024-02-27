0f4a2e3cbbeb3a6c84fc3ca6efcdfe0acc7e9360
Andyyyy987 <am38387@gmail.com>
2024-02-27 08:24:57 +0000

    updates to auth/controllers/routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require("cors");
 const User = require("./users/model");
 
 const userRouter = require("./users/routes");
@@ -9,15 +9,45 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use(userRouter);
 
-const syncTables = () => {
-    User.sync();
+//==================== middleware example ==================
+
+const dave = async (...args) => {};
+
+const sendRes = async (req, res) => {
+  res.send({ data: req.body });
 };
 
-app.listen(port, ()=> {
-    syncTables();
-    console.log(`Server is listening on port ${port}`);
+const justReq = async (req) => {
+  console.log(req.body);
+};
+
+const firstFunc = async (req, res, next) => {
+  req.body.first = "one";
+
+  next();
+};
+
+const secondFunc = async (req, res, next) => {
+  req.body.second = "two";
+
+  next();
+};
+
+app.post("/middlewareexample", firstFunc, secondFunc, sendRes);
+
+//==========================================================
+
+const syncTables = () => {
+  User.sync();
+};
+
+app.listen(port, () => {
+  syncTables();
+  console.log(`Server is listening on port ${port}`);
 });
